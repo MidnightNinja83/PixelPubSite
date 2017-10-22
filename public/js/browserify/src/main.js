a9bf461194eaf1f9560448a9dbbad8f71308a323
Bloodrave120 <Bloodrave120@hotmail.com>
2017-10-22 16:57:34 -0400

    added media player, resolved style bugs (the crew page), added new style to add toggle switch and play/pause fade

--- public/js/browserify/src/main.js
+++ public/js/browserify/src/main.js
@@ -1,4 +1,19 @@
 require('waypoints/lib/jquery.waypoints.min');
+require('howler/dist/howler');
+
+var wtnPlaylist = new Howl({
+  src: ['../media/wtnpodcast/ep10-destiny2-giveaway.mp3']
+});
+
+function togglePlay() {
+	var $elem = $('#player').children(':first');
+	$elem.stop()
+	.show()
+	.animate({'marginTop':'-75px','marginLeft':'-75px','width':'50px','height':'50px','opacity':'0'}, () => {
+		$elem.css({'width':'100px','height':'100px','margin-left':'-50px','margin-top':'-50px','opacity':'1','display':'none'});
+	});
+	$elem.parent().append($elem);
+}
 
 var gameIconsTable = $('#ppIntroTbl').waypoint({
 	handler: function(direction) {
@@ -9,38 +24,12 @@ var gameIconsTable = $('#ppIntroTbl').waypoint({
 	offset: 250
 });
 
-var firstProfile = $('#ppb').waypoint({
-	handler: function(direction) {
-		if (direction === 'down') {
-			$('#ppProfileTblRow1').show('slide');
-		}
-	},
-	offset: -50
-});
-
-var secondProfile = $('#ppb').waypoint({
-	handler: function(direction) {
-		if (direction === 'down') {
-			$('#ppProfileTblRow2').show('slide');
-		}
-	},
-	offset: -300
-});
-
-var thirdProfile = $('#ppb').waypoint({
-	handler: function(direction) {
-		if (direction === 'down') {
-			$('#ppProfileTblRow3').show('slide');
-		}
-	},
-	offset: -700
-});
-
-var fourthProfile = $('#ppb').waypoint({
-	handler: function(direction) {
-		if (direction === 'down') {
-			$('#ppProfileTblRow4').show('slide');
-		}
-	},
-	offset: -1100
+$('#playSlider').change(() => {
+	if ($('#playSlider')[0].checked) {
+		wtnPlaylist.play();
+	}
+	else {
+		wtnPlaylist.pause();
+	}
+	togglePlay();
 });
