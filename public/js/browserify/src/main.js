a6c84a69ba541fe5b87f993844a5a692860a21fe
Bloodrave120 <Bloodrave120@hotmail.com>
2017-11-17 19:18:46 -0500

    wtn podcast ep 14

--- public/js/browserify/src/main.js
+++ public/js/browserify/src/main.js
@@ -2,7 +2,7 @@ require('waypoints/lib/jquery.waypoints.min');
 require('howler/dist/howler');
 
 var wtnPlaylist = new Howl({
-  src: ['../media/wtnpodcast/ep13-eye-of-the-blizzard.mp3'],
+  src: ['../media/wtnpodcast/ep14-the-tragedy-of-ea-the-greedy.mp3'],
   html5: true,
   buffer: true,
   onloaderror: logErr
