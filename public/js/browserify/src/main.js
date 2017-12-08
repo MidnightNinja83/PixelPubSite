4c9c60a19b7830f3ba44b9bb9575b19707693ec2
Bloodrave120 <Bloodrave120@hotmail.com>
2017-12-07 19:46:44 -0500

    ep. 16 of the WTN podcast

--- public/js/browserify/src/main.js
+++ public/js/browserify/src/main.js
@@ -2,7 +2,7 @@ require('waypoints/lib/jquery.waypoints.min');
 require('howler/dist/howler');
 
 var wtnPlaylist = new Howl({
-  src: ['../media/wtnpodcast/ep15-two-tokens-and-a-podcast.mp3'],
+  src: ['../media/wtnpodcast/ep16-top-5-games-of-the-year.mp3'],
   html5: true,
   buffer: true,
   onloaderror: logErr
