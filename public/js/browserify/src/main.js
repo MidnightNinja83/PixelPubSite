04dbccfd0be0dbe803a9ca9622b0589d9e7352c6
Bloodrave120 <Bloodrave120@hotmail.com>
2017-12-14 18:14:45 -0500

    ep17 WTN podcast

--- public/js/browserify/src/main.js
+++ public/js/browserify/src/main.js
@@ -2,7 +2,7 @@ require('waypoints/lib/jquery.waypoints.min');
 require('howler/dist/howler');
 
 var wtnPlaylist = new Howl({
-  src: ['../media/wtnpodcast/ep16-top-5-games-of-the-year.mp3'],
+  src: ['../media/wtnpodcast/ep17-twas-the-night-before-star-wars.mp3'],
   html5: true,
   buffer: true,
   onloaderror: logErr
