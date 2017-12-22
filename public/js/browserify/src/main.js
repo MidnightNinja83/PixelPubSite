93ac1a4c9db02a05261ff5b2505ea0e9e5acab9b
Bloodrave120 <Bloodrave120@hotmail.com>
2017-12-21 21:03:02 -0500

    wtn podcast ep 18

--- public/js/browserify/src/main.js
+++ public/js/browserify/src/main.js
@@ -2,7 +2,7 @@ require('waypoints/lib/jquery.waypoints.min');
 require('howler/dist/howler');
 
 var wtnPlaylist = new Howl({
-  src: ['../media/wtnpodcast/ep17-twas-the-night-before-star-wars.mp3'],
+  src: ['../media/wtnpodcast/ep18-deck-the-halls-with-games.mp33'],
   html5: true,
   buffer: true,
   onloaderror: logErr
