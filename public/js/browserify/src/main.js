eed3cb6ec45ace1dd10cf70992f3814936ed6a65
Bloodrave120 <Bloodrave120@hotmail.com>
2017-12-28 18:44:33 -0500

    ep 19 wtn podcast

--- public/js/browserify/src/main.js
+++ public/js/browserify/src/main.js
@@ -2,7 +2,7 @@ require('waypoints/lib/jquery.waypoints.min');
 require('howler/dist/howler');
 
 var wtnPlaylist = new Howl({
-  src: ['../media/wtnpodcast/ep18-deck-the-halls-with-games.mp3'],
+  src: ['../media/wtnpodcast/ep19-goodbye-2017.mp3'],
   html5: true,
   buffer: true,
   onloaderror: logErr
