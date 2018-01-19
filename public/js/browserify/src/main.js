7c48891dcc6f42c77ff805652228acef2c80eb53
Bloodrave120 <Bloodrave120@hotmail.com>
2018-01-18 19:07:42 -0500

    ep 22 wtn podcast

--- public/js/browserify/src/main.js
+++ public/js/browserify/src/main.js
@@ -2,7 +2,7 @@ require('waypoints/lib/jquery.waypoints.min');
 require('howler/dist/howler');
 
 var wtnPlaylist = new Howl({
-  src: ['../media/wtnpodcast/ep21-happy-birthday-jared.mp3'],
+  src: ['../media/wtnpodcast/ep22-nintendo-labo-cardboard-of-the-future.mp3'],
   html5: true,
   buffer: true,
   onloaderror: logErr
