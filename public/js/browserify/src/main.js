32764ae92cd2bb236f484abdae87c4caa520d7df
Bloodrave120 <Bloodrave120@hotmail.com>
2018-03-03 13:23:08 -0500

    ep. 28 - wtn podcast

--- public/js/browserify/src/main.js
+++ public/js/browserify/src/main.js
@@ -2,7 +2,7 @@ require('waypoints/lib/jquery.waypoints.min');
 require('howler/dist/howler');
 
 var wtnPlaylist = new Howl({
-  src: ['../media/wtnpodcast/ep27-black-panther-topping-charts.mp3'],
+  src: ['../media/wtnpodcast/ep28-we-jump-into-the-breach.mp3'],
   html5: true,
   buffer: true,
   onloaderror: logErr
