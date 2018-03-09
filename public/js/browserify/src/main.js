2cc6ce0d7ceccc90cefcb1f99c7b15d9fff63f2d
Bloodrave120 <Bloodrave120@hotmail.com>
2018-03-08 20:32:00 -0500

    ep 29 - wtn podcast

--- public/js/browserify/src/main.js
+++ public/js/browserify/src/main.js
@@ -2,7 +2,7 @@ require('waypoints/lib/jquery.waypoints.min');
 require('howler/dist/howler');
 
 var wtnPlaylist = new Howl({
-  src: ['../media/wtnpodcast/ep28-we-jump-into-the-breach.mp3'],
+  src: ['../media/wtnpodcast/ep29-sea-of-thieves-open-beta-maybe'],
   html5: true,
   buffer: true,
   onloaderror: logErr
