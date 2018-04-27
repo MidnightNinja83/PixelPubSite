8e6301fcdaadaae9942166c96d92343416662ecb
Andrew Mazza <bloodrave120@hotmail.com>
2018-04-26 20:40:39 -0400

    ep. 36 wtn podcast

--- public/js/browserify/src/main.js
+++ public/js/browserify/src/main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 });
 
 var wtnPlaylist = new Howl({
-  src: ['../media/wtnpodcast/ep35-the-next-fortnite-coming-soon.mp3'],
+  src: ['../media/wtnpodcast/ep36-we-talk-god-of-war.mp3'],
   html5: true,
   buffer: true,
   onloaderror: logErr
